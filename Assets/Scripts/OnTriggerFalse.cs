7f0fbd470690e475f76adf2ba3d54a775b39dd4e
Mert Arar <85193329+MertArar@users.noreply.github.com>
2024-05-02 18:38:17 +0300

    Coin count added in UI

--- Assets/Scripts/OnTriggerFalse.cs
+++ Assets/Scripts/OnTriggerFalse.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class OnTriggerFalse : MonoBehaviour
 {
+    public AudioClip coinSound;
+    
     private void OnTriggerEnter(Collider other)
     {
+        AudioSource.PlayClipAtPoint(coinSound, transform.position);
+        CollectableControl.coinCount += 1;
         gameObject.SetActive(false);
     }
 }
